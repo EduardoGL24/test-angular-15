0db04d61c6d6eaab97becda93145e08977fab673
Eduardo García <eduardo.gl24@hotmail.com>
2023-09-09 23:32:36 -0600

    add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './auth/login/login.component';
-import { ConversionsComponent } from './pages/conversions/conversions.component';
-import { CalculateDateComponent } from './pages/calculate-date/calculate-date.component';
-import { FormComponent } from './pages/form/form.component';
-
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
-  { path: 'home', component: HomeComponent },
-  { path: 'conversiones', component: ConversionsComponent },
-  { path: 'calcular-fecha', component: CalculateDateComponent},
-  { path: 'formulario', component: FormComponent}
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomeModule) },
+  { path: 'conversiones', loadChildren: () => import('./pages/conversions/conversions.module').then( m => m.ConversionsModule) },
+  { path: 'calcular-fecha', loadChildren: () => import('./pages/calculate-date/calculate-date.module').then( m => m.CalculateDateModule)},
+  { path: 'formulario', loadChildren: () => import('./pages/form/form.module').then( m => m.FormModule)}
 ];
 
 @NgModule({
